fe2d639b0a881f63c153f9ee3e1ce3547dffe119
zpjayyy <jay.lichzp@gmail.com>
2024-03-14 15:50:16 +0800

    feat: route cache

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -1,9 +1,8 @@
 import {NextResponse} from "next/server";
 
 export async function GET() {
-  const res = await fetch('https://api.thecatapi.com/v1/images/search', {
-    next: {revalidate: 5}
-  })
+  const signal: AbortSignal = new AbortSignal()
+  const res = await fetch('https://api.thecatapi.com/v1/images/search', {signal})
   const data = await res.json()
   console.log(data)
   return NextResponse.json(data)
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
 import {photos} from "./data";
 import Link from "next/link";
 
+async function handleFormAction(formData: FormData) {
+  'use server'
+  const name = formData.get('name')
+}
+
 export default function Home() {
   return (
-    <div className="flex flex-row flex-wrap">
-      {photos.map(({id, src}) => (
-        <Link key={id} href={`/photo/${id}`}>
-          <img width="200" src={src} className="m-1" alt="Cat"/>
-        </Link>
-      ))}
-    </div>
+    <>
+      <div className="flex flex-row flex-wrap">
+        {photos.map(({id, src}) => (
+          <Link key={id} href={`/photo/${id}`}>
+            <img width="200" src={src} className="m-1" alt="Cat"/>
+          </Link>
+        ))}
+      </div>
+      <div>
+        <form action={handleFormAction}>
+          <input type="text" name="name"/>
+          <button type="submit">submit</button>
+        </form>
+      </div>
+    </>
   );
 }
